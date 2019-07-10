ef6a746e40473502df4ce046a862a5f527ebb9d7
vincent poon <boy19970722@hotmail.com>
2019-07-09 20:36:28 -0500

    added filter funtion for tags

--- TextProcessing.py
+++ TextProcessing.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import nltk
-import nltk.data
 """
 Spyder Editor
 
@@ -14,15 +13,24 @@ This is a temporary script file.
     dafault path are in \AppData\Roaming\nltk_data
     Search '%appdata%' will open up roaming folder
 """
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+class keyWordFilter:
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
+    
+    def tokenizeString(string):
+        tokens = nltk.word_tokenize(string)
+        return tokens
+    
+    def tagTokens(tokens):
+        taggedTokens = nltk.pos_tag(tokens)
+        return taggedTokens
 
+    def filterTagAndSelectTokens(string):
+        tokens = keyWordFilter.tokenizeString(string)
+        taggedTokens = keyWordFilter.tagTokens(tokens)
+        b = list(filter(lambda a: a[1] != 'NNP', taggedTokens))
+        print(b)
+        
 
-sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
-tokens = nltk.word_tokenize(sentence)
-
-print(tokens)
-
-taggedTokens = nltk.pos_tag(tokens)
-
-print(taggedTokens)
+keyWordFilter.filterTagAndSelectTokens('Strong Python/shell scripting experience')
