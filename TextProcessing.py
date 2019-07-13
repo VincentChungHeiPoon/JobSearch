ae3cd3c8787467d6d40cdb6cb56d91ea1354c8e0
vincent poon <boy19970722@hotmail.com>
2019-07-12 20:37:19 -0500

    added counter for the word filter

--- TextProcessing.py
+++ TextProcessing.py
@@ -13,16 +13,19 @@ This is a temporary script file.
     dafault path are in \AppData\Roaming\nltk_data
     Search '%appdata%' will open up roaming folder
 """
-class keyWordFrequency:
+class keyWord:
     def __init__(self, word):
         self.word = word
-        self.count = 0
+        self.count = 1
 
 
 class keyWordFilter:
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
     nltk.download('wordnet')
+    
+    def __init__(self):
+        self.wordFrequencyList = []
     
     def tokenizeString(string):
         tokens = nltk.word_tokenize(string)
@@ -36,7 +39,19 @@ class keyWordFilter:
         tokens = keyWordFilter.tokenizeString(string)
         taggedTokens = keyWordFilter.tagTokens(tokens)
         filteredList = list(filter(lambda a: a[1] == 'NNP' or a[1] == 'NN' or a[1] == 'JJ', taggedTokens))
-        print(filteredList)
-        
+        return filteredList
     
-keyWordFilter.filterTagAndSelectTokens('As a member of our Software Engineering Group we look first and foremost for people who are passionate around solving business problems through innovation & engineering practices. You will be required to apply your depth of knowledge and expertise to all aspects of the software development lifecycle, as well as partner continuously with your many stakeholders on a daily basis to stay focused on common goals. We embrace a culture of experimentation and constantly strive for improvement and learning. You’ll work in a collaborative, trusting, thought-provoking environment—one that encourages diversity of thought and creative solutions that are in the best interests of our customers globally.')
+    def addWordToList(self, word):
+        existList = [words for words in self.wordFrequencyList if words.word == word]
+        if len(existList) == 1: 
+            #print(self.wordFrequencyList.index(word))
+            self.wordFrequencyList[self.wordFrequencyList.index(existList[0])].count += 1
+        else:
+            self.wordFrequencyList.append(keyWord(word))
+        
+
+string = 'As a member of our Software Engineering Group we look first and foremost for people who are passionate around solving business problems through innovation & engineering practices. You will be required to apply your depth of knowledge and expertise to all aspects of the software development lifecycle, as well as partner continuously with your many stakeholders on a daily basis to stay focused on common goals. We embrace a culture of experimentation and constantly strive for improvement and learning. You’ll work in a collaborative, trusting, thought-provoking environment—one that encourages diversity of thought and creative solutions that are in the best interests of our customers globally.'
+a = keyWordFilter()
+a.addWordToList('hi')
+a.addWordToList('2')
+a.addWordToList('2')
