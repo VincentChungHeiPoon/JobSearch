40a35a3e8d207db2318b9bc57399e76dcd86ee6e
vincent poon <boy19970722@hotmail.com>
2019-07-14 16:11:24 -0500

    added beautifulsoup4 to dependency

---
+++ RequirementScraper.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 16:03:11 2019
+
+@author: Vincent Poon
+
+This file contains all functions needed to scrap the following list of website:
+    
+It will return text from the website for further processing
+"""
+
