d2a18bae8ff70e19af09c98e25b6f5c187222446
vincent poon <boy19970722@hotmail.com>
2019-07-29 18:45:19 -0500

    Finished get url function

--- RequirementScraper.py
+++ RequirementScraper.py
@@ -11,17 +11,7 @@ It will return text from the website for further processing
 
 from bs4 import BeautifulSoup
 import requests
-
-
-url = "https://www.indeed.com/jobs?q=software%20developer&l=Houston%2C%20TX&oq=software%20developer%20%2490%2C000%1Fsoftware%20developer&ol=Houston%2C%20TX%1FHouston%2C%20TX&ts=1563488118807&pts=1563231640432&rq=1&fromage=last&advn=122089296871042&vjk=83e3314be5a6cc70"
-result = requests.get(url)
-
-soup = BeautifulSoup(result.text, 'htmlParser')
-#print(soup.prettify())
-#print(soup.find_all('table', id = 'pageContent'))
-#print(soup.find_all('td', id = 'auxCol'))
-print(soup.find_all('div', id = 'vjs-header'))
-print(soup.find_all('p'))
+import WebBrowserController
 
 '''
 indeed.com
--- seleniumAutoUser.py
+++ seleniumAutoUser.py
@@ -14,11 +14,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-class webBrowserController:
+class WebBrowserController:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path = r'C:\Users\boy19\Desktop\chromedriver_win32\chromedriver.exe')
         self.indeed = 'https://www.indeed.com'
-        self.content = ''
+        self.urlList = []
         
     def getRequirementHtml(self, requirementUrl):
         self.driver.get(requirementUrl);
@@ -27,7 +27,16 @@ class webBrowserController:
         
     def getAllPostingURL(self, title, location):
         self.indeedSearch(title, location)
-    
+        source = self.driver.page_source
+        soup = BeautifulSoup(source, 'html.parser')
+        for jobCard in soup.find_all('div', class_ = 'jobsearch-SerpJobCard unifiedRow row result clickcard'):
+            for title in jobCard.find_all('div', class_ = 'title'):
+                for url in title.find_all('a'):     
+                    #append www.indeed.com to url to get full URL
+                    self.urlList.append(self.indeed + str(url['href']))
+            
+        
+    #input and search
     def indeedSearch(self, title, location):
         self.driver.get(self.indeed)
         titleInput = self.driver.find_element_by_id('text-input-what')
@@ -49,8 +58,9 @@ class webBrowserController:
         
 url = 'https://www.indeed.com/jobs?q=software%20developer&l=Houston%2C%20TX&oq=software%20developer%20%2490%2C000%1Fsoftware%20developer&ol=Houston%2C%20TX%1FHouston%2C%20TX&ts=1563998196080&pts=1563899770218&rq=1&fromage=last&advn=357631224894577&vjk=78d24a7642cfbfd9'
 searchResult = 'https://www.indeed.com/jobs?q=software%20developer&l=Houston%2C%20TX&oq=software%20developer%20%2490%2C000%1Fsoftware%20developer&ol=Houston%2C%20TX%1FHouston%2C%20TX&ts=1563998196080&pts=1563899770218&rq=1&fromage=last&advn=357631224894577&vjk=78d24a7642cfbfd9'
-a = webBrowserController()
+a = WebBrowserController()
 #a.getRequirementHtml(url)
 #print(a.content)
 
 a.getAllPostingURL('software developer', 'houston')
+print(a.urlList)
